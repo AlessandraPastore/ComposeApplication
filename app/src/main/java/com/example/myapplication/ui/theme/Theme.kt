3c5f963636df8ba29b2a5a162079da60910f8945
Alessandra <pastore.alessandra@live.it>
2021-05-13 14:19:25 +0200

    theme

--- app/src/main/java/com/example/myapplication/ui/theme/Theme.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Theme.kt
@@ -3,14 +3,15 @@ package com.example.myapplication.ui.theme
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 
 
 @Composable
 fun MyApplicationTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: MutableState<Boolean>,
     content: @Composable() () -> Unit
 ) {
-    val colors = if (darkTheme) {
+    val colors = if (darkTheme.value) {
         DarkColors
     } else {
         LightColors
