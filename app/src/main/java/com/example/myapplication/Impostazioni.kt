cb50c582565d0fd49571552bb211e6bdaaa24f8e
Alessandra <pastore.alessandra@live.it>
2021-05-13 18:32:43 +0200

    settings

--- app/src/main/java/com/example/myapplication/Impostazioni.kt
+++ app/src/main/java/com/example/myapplication/Impostazioni.kt
@@ -1,11 +1,17 @@
 package com.example.myapplication
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 
@@ -21,8 +27,14 @@ fun Impostazioni(enableDarkMode: MutableState<Boolean>) {
         //Text(stringResource(R.string.impostazioni))
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxSize()
+
+
+
         ){
-            //Icon(Icons.Rounded.Favorite, "")
+            //Icon(Icons.Rounded., "") non trovo le icone carine
             Text(
                 text = "clicca per cambiare modalità",
                 textAlign = TextAlign.Center
