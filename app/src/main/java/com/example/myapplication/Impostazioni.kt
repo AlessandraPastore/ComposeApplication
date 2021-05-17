eece37220e8f7a06baae07d19d1b4b264d85dbcb
Alessandra <pastore.alessandra@live.it>
2021-05-17 16:45:34 +0200

    sun/moon

--- app/src/main/java/com/example/myapplication/Impostazioni.kt
+++ app/src/main/java/com/example/myapplication/Impostazioni.kt
@@ -2,18 +2,20 @@ package com.example.myapplication
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Light
+import androidx.compose.material.icons.rounded.LightMode
+import androidx.compose.material.icons.rounded.ModeNight
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Impostazioni(enableDarkMode: MutableState<Boolean>) {
@@ -24,7 +26,7 @@ fun Impostazioni(enableDarkMode: MutableState<Boolean>) {
             })
         }
     ){
-        //Text(stringResource(R.string.impostazioni))
+
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center,
@@ -34,7 +36,11 @@ fun Impostazioni(enableDarkMode: MutableState<Boolean>) {
 
 
         ){
-            //Icon(Icons.Rounded., "") non trovo le icone carine
+            if(enableDarkMode.value)    Icon(Icons.Rounded.ModeNight, contentDescription = "", modifier = Modifier.scale(2F))
+            else Icon(Icons.Rounded.LightMode, contentDescription = "", modifier = Modifier.scale(2F))
+
+            Spacer(modifier = Modifier.height(20.dp))
+            
             Text(
                 text = "clicca per cambiare modalità",
                 textAlign = TextAlign.Center
